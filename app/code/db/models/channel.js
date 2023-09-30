1a51cec6245b8b28b4b5e2ea4ad40092fd1d1d49
Max Golubev <imagickey@icloud.com>
2023-10-01 00:45:41 +0200

    1

--- app/code/db/models/channel.js
+++ app/code/db/models/channel.js
@@ -1,22 +1,42 @@
 import BaseModel from './base-model'
 
 export default class Channel extends BaseModel {
-    constructor(data, created = true) {
-        const fields = {
-            id: null,
-            channel_id: null,
-            display_name: null,
-            platform: null,
-            image_url: null,
-            blocked: null,
-            foreign_id: null,
-            num: 10,
-        }
-        super(fields, data, created)
+    constructor(data) {
+        super(data)
     }
 
     static fields = {
-        id: { index: true, default: 10, indexed: true, type: BaseModel.STRING},
+        id: {
+            type: BaseModel.NUMBER,
+        },
+        channel_id: {
+            type: BaseModel.STRING,
+            index: true,
+        },
+        display_name: {
+            type: BaseModel.STRING,
+            index: true,
+        },
+        platform: {
+            type: BaseModel.STRING,
+            index: true,
+        },
+        image_url: {
+            type: BaseModel.STRING,
+        },
+        blocked: {
+            type: BaseModel.STRING,
+            index: true,
+        },
+        foreign_id: {
+            type: BaseModel.STRING,
+            index: true,
+        },
+        num: {
+            type: BaseModel.NUMBER,
+            default: 10,
+            index: true,
+        },
     }
 
     static prefix = 'channels'
