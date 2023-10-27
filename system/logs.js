cc613f17d892d54844540729fb791461e0d8ca07
Max Golubev <imagickey@icloud.com>
2023-10-27 14:19:01 +0200

    Added clear logs

---
+++ system/logs.js
@@ -0,0 +1,11 @@
+import { writeFileSync, existsSync } from 'fs'
+import { resolve } from 'path'
+
+const logs = process.argv.slice(2)
+const ROOT_PATH = resolve()
+const LOG_LEVELS = ['log', 'debug', 'error', 'warn', 'info', 'crit', 'service-output', 'service-error']
+
+LOG_LEVELS.forEach((logLevel) => {
+    const filePath = resolve(ROOT_PATH, 'app', 'logs', `${logLevel}.log`)
+    if (existsSync(filePath) && (!logs.length || logs.includes(logLevel))) writeFileSync(filePath, '')
+})
