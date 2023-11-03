d9e5fd42dd98872178b901f2d453e93d90b73405
Max Golubev <imagickey@icloud.com>
2023-11-03 09:02:09 +0100

    Seporate load config

--- app/index.js
+++ app/index.js
@@ -1,23 +1,8 @@
-import fs from 'fs'
 import run from './code'
-import utils from './libs/utils'
 import Logger from './libs/logger'
+import getConfig from './libs/get-config'
 
-global.config = {}
-const CONFIG_DIR = `./app/config/${process.env.NODE_ENV}`
-const ENTRIES = fs.readdirSync(CONFIG_DIR)
-const CONFIG_FILES = ENTRIES.filter((val) => val.slice(-5) === '.json')
-
-CONFIG_FILES.forEach((configFile) => {
-    const configName = configFile.slice(0, -5).toLowerCase()
-    const configData = fs.readFileSync(`${CONFIG_DIR}/${configFile}`)
-
-    if (utils.isJSON(configData) === true) {
-        global.config[configName] = JSON.parse(configData)
-    } else {
-        throw Error(`Can't parse JSON config: ${CONFIG_DIR}/${configFile}`)
-    }
-})
+global.config = getConfig()
 
 Logger.setConfig(global.config.logger)
 global.logger = Logger
---
+++ app/libs/get-config.js
@@ -0,0 +1,23 @@
+import fs from 'fs'
+import utils from './utils'
+
+export default () => {
+    const config = {}
+
+    const CONFIG_DIR = `./app/config/${process.env.NODE_ENV}`
+    const ENTRIES = fs.readdirSync(CONFIG_DIR)
+    const CONFIG_FILES = ENTRIES.filter((val) => val.slice(-5) === '.json')
+
+    CONFIG_FILES.forEach((configFile) => {
+        const configName = configFile.slice(0, -5).toLowerCase()
+        const configData = fs.readFileSync(`${CONFIG_DIR}/${configFile}`)
+
+        if (utils.isJSON(configData) === true) {
+            config[configName] = JSON.parse(configData)
+        } else {
+            throw Error(`Can't parse JSON config: ${CONFIG_DIR}/${configFile}`)
+        }
+    })
+
+    return config
+}
